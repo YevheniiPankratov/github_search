aaf9f4fa81a42523173eb8d0bb9451fd6fc8184d
YevheniiPankratov <yevhenii.pankratov@nure.ua>
2021-03-24 21:22:35 +0200

    finished Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { AlertContext } from '../context/alert/alertContext';
 
-export const Alert = ({alert}) => {
+export const Alert = () => {
+    const {alert, hide,} = useContext(AlertContext)
     
+    if (!alert) return null
+
     return(
         <div>
             <div className={`alert alert-${alert.type || 'dark'} alert-dismissible`} role="alert">
                 {alert.text}
-                <button type="button" class="close" aria-label="Close">
+                <button type="button" className="close" aria-label="Close" onClick={hide}>
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,24 @@
-import React from 'react';
-
+import React, {useContext, useState} from 'react';
+import { AlertContext } from '../context/alert/alertContext';
 export const Search = () => {
+    const [value, setValue] = useState('')
+    const {show} = useContext(AlertContext)
+    
+    const onSubmit = (e) => {
+        if (e.key !== 'Enter') {
+            return
+        }
+
+        if (value.trim()) {
+            console.log(value)
+        } else {
+            show('Enter user data')
+        }
+    }
     return (
             <div className="input-group flex-nowrap mb-3">
             <span className="input-group-text" id="addon-wrapping">@</span>
-            <input type="text" className="form-control" placeholder="Enter username..." aria-label="Username" aria-describedby="addon-wrapping"/>
+            <input type="text" className="form-control" placeholder="Enter username..." aria-label="Username" aria-describedby="addon-wrapping" onKeyPress={onSubmit} value={value} onChange={e => setValue(e.target.value)}/>
             </div>
     )
 }
